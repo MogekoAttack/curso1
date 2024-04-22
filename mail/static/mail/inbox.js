07e6c521b5ba5ebc6ffcd8c43ef04a21e391a4e9
MogekoAttack <miguel.angel.rgz.74@gmail.com>
2024-04-22 00:51:48 -0600

    Primer punto hecho.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  document.querySelector('#compose-form').addEventListener('submit', Enviar);
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -30,4 +30,28 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function Enviar(evt) {
+  evt.preventDefault();
+
+  // Obtén los valores del formulario
+  let destinatario = document.querySelector('#compose-recipients').value;
+  let asunto = document.querySelector('#compose-subject').value;
+  let mensaje = document.querySelector('#compose-body').value;
+
+  // Haz una solicitud POST a '/emails'
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: destinatario,
+        subject: asunto,
+        body: mensaje,
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
 }
